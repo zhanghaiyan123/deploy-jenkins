fb8be05f93bd6c88d26a259581a924e260512091
zhanghy <270750596@qq.com>
2019-10-18 16:37:30 +0800

    改注释

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const isProductEnv = process.env.NODE_ENV === 'production'
 module.exports = {
   isProductEnv,
   staticPath: 'public',
-  // 数据库连接
+  // 端口配置
   port: 3201,
   defaultFePort: 3201
 }
